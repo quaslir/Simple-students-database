77455da02b098b634bc7c9fab2b729df7b53ccd3
Daniil <46322149+quaslir@users.noreply.github.com>
2025-11-18 21:13:29 +0100

    Remove GREEN color definition from students.c

--- students.c
+++ students.c
@@ -6,7 +6,6 @@
 const char *FILENAME = "Students.txt";  
 #define RESET       "\033[0m"
 #define RED         "\033[0;031m"
-#define GREEN       "\033[0;31m"
 #define YELLOW      "\033[0;33m"
 #define BLUE        "\033[0;34m"
 #define CYAN        "\033[0;36m"
